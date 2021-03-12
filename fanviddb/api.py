68e028e325fa107781974d5c4ded32f851d83e15
sandalwoodbox <boxofsandalwood@gmail.com>
2021-03-11 18:03:23 -0800

    Blackified codebase

--- fanviddb/api.py
+++ fanviddb/api.py
@@ -17,11 +17,13 @@ async def read_root():
 @app.post("/fanvids", response_model=Fanvid, tags=["Fanvids"])
 async def create_fanvid(fanvid: CreateOrUpdateFanvid, status_code=201):
     fanvid_dict = fanvid.dict()
-    fanvid_dict.update({
-        'uuid': uuid.uuid4(),
-        "created_timestamp": datetime.datetime.utcnow(),
-        "modified_timestamp": datetime.datetime.utcnow(),
-    })
+    fanvid_dict.update(
+        {
+            "uuid": uuid.uuid4(),
+            "created_timestamp": datetime.datetime.utcnow(),
+            "modified_timestamp": datetime.datetime.utcnow(),
+        }
+    )
     return fanvid_dict
 
 
@@ -29,24 +31,24 @@ async def create_fanvid(fanvid: CreateOrUpdateFanvid, status_code=201):
 async def read_fanvid_list():
     return [
         {
-           "uuid": uuid.uuid4(),
-           "title": "Title",
-           "author": "Author",
-           "premiere_date": None,
-           "premiere_event": None,
-           "audio": None,
-           "length": datetime.timedelta(1),
-           "rating": "Gen",
-           "fandoms": [],
-           "summary": "Summary!",
-           "content_notes": ["Graphic depictions of violence"],
-           "urls": ["https://archiveofourown.org/works/29759784"],
-           "unique_identifiers": [],
-           "thumbnail_url": "https://i.vimeocdn.com/video/1072432518.jpg?mw=1280&mh=720&q=70",
-           "state": "active",
-           "created_timestamp": datetime.datetime.utcnow(),
-           "modified_timestamp": datetime.datetime.utcnow(),
-       },
+            "uuid": uuid.uuid4(),
+            "title": "Title",
+            "author": "Author",
+            "premiere_date": None,
+            "premiere_event": None,
+            "audio": None,
+            "length": datetime.timedelta(1),
+            "rating": "Gen",
+            "fandoms": [],
+            "summary": "Summary!",
+            "content_notes": ["Graphic depictions of violence"],
+            "urls": ["https://archiveofourown.org/works/29759784"],
+            "unique_identifiers": [],
+            "thumbnail_url": "https://i.vimeocdn.com/video/1072432518.jpg?mw=1280&mh=720&q=70",
+            "state": "active",
+            "created_timestamp": datetime.datetime.utcnow(),
+            "modified_timestamp": datetime.datetime.utcnow(),
+        },
     ]
 
 
@@ -76,11 +78,13 @@ async def read_fanvid(fanvid_uuid: uuid.UUID):
 @app.put("/fanvids/{fanvid_uuid}", response_model=Fanvid, tags=["Fanvids"])
 async def update_fanvid(fanvid_uuid: uuid.UUID, fanvid: CreateOrUpdateFanvid):
     fanvid_dict = fanvid.dict()
-    fanvid_dict.update({
-        'uuid': fanvid_uuid,
-        "created_timestamp": datetime.datetime.utcnow(),
-        "modified_timestamp": datetime.datetime.utcnow(),
-    })
+    fanvid_dict.update(
+        {
+            "uuid": fanvid_uuid,
+            "created_timestamp": datetime.datetime.utcnow(),
+            "modified_timestamp": datetime.datetime.utcnow(),
+        }
+    )
     return fanvid_dict
 
 
--- fanviddb/models.py
+++ fanviddb/models.py
@@ -6,32 +6,34 @@ from pydantic import BaseModel
 
 
 class Audio(BaseModel):
-	title: str
-	creators: List[str]
+    title: str
+    creators: List[str]
 
 
 class BaseFanvid(BaseModel):
-	title: str
-	creators: List[str]
-	premiere_date: Optional[datetime.date] = None
-	premiere_event: Optional[str] = None
-	audio: Optional[Audio] = None
-	length: datetime.timedelta
-	rating: str
-	fandoms: List[str] = []
-	summary: str
-	content_notes: List[str] = []
-	urls: List[str] = []
-	unique_identifiers: List[str] = []
-	thumbnail_url: str
+    title: str
+    creators: List[str]
+    premiere_date: Optional[datetime.date] = None
+    premiere_event: Optional[str] = None
+    audio: Optional[Audio] = None
+    length: datetime.timedelta
+    rating: str
+    fandoms: List[str] = []
+    summary: str
+    content_notes: List[str] = []
+    urls: List[str] = []
+    unique_identifiers: List[str] = []
+    thumbnail_url: str
 
-	# Admin-only
-	state: str
+    # Admin-only
+    state: str
+
 
 class CreateOrUpdateFanvid(BaseFanvid):
-	pass
+    pass
+
 
 class Fanvid(BaseFanvid):
-	uuid: uuid.UUID
-	created_timestamp: datetime.datetime
-	modified_timestamp: datetime.datetime
+    uuid: uuid.UUID
+    created_timestamp: datetime.datetime
+    modified_timestamp: datetime.datetime
