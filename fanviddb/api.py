35d5659feafbdbd343c2135d3a9a17db6096ff6f
sandalwoodbox <boxofsandalwood@gmail.com>
2021-03-13 07:21:45 -0800

    Added html root page

--- fanviddb/api.py
+++ fanviddb/api.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from starlette.routing import Route
+from starlette.staticfiles import StaticFiles
 
 from .db import database
 from .fanvids.router import router as fanvid_router
 
-app = FastAPI()
+app = FastAPI(
+    routes=[
+        Route("/", endpoint=StaticFiles(directory="static", html=True), name="homepage")
+    ],
+)
 app.include_router(
     fanvid_router,
     prefix="/fanvids",
@@ -19,8 +25,3 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
