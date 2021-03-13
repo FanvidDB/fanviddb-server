220efe008b6771bb9cbd23072a41c95e42bdc848
sandalwoodbox <boxofsandalwood@gmail.com>
2021-03-12 17:14:18 -0800

    Moved fanvids-related code into a subfolder

--- fanviddb/api.py
+++ fanviddb/api.py
@@ -1,93 +1,15 @@
-import datetime
-import uuid
-from typing import List, Optional
-
 from fastapi import FastAPI
 
-from fanviddb.models import CreateOrUpdateFanvid, Fanvid
+from .fanvids.router import router as fanvid_router
 
 app = FastAPI()
+app.include_router(
+    fanvid_router,
+    prefix="/fanvids",
+    tags=["Fanvids"],
+)
 
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-
-@app.post("/fanvids", response_model=Fanvid, tags=["Fanvids"])
-async def create_fanvid(fanvid: CreateOrUpdateFanvid, status_code=201):
-    fanvid_dict = fanvid.dict()
-    fanvid_dict.update(
-        {
-            "uuid": uuid.uuid4(),
-            "created_timestamp": datetime.datetime.utcnow(),
-            "modified_timestamp": datetime.datetime.utcnow(),
-        }
-    )
-    return fanvid_dict
-
-
-@app.get("/fanvids", response_model=List[Fanvid], tags=["Fanvids"])
-async def read_fanvid_list():
-    return [
-        {
-            "uuid": uuid.uuid4(),
-            "title": "Title",
-            "author": "Author",
-            "premiere_date": None,
-            "premiere_event": None,
-            "audio": None,
-            "length": datetime.timedelta(1),
-            "rating": "Gen",
-            "fandoms": [],
-            "summary": "Summary!",
-            "content_notes": ["Graphic depictions of violence"],
-            "urls": ["https://archiveofourown.org/works/29759784"],
-            "unique_identifiers": [],
-            "thumbnail_url": "https://i.vimeocdn.com/video/1072432518.jpg?mw=1280&mh=720&q=70",
-            "state": "active",
-            "created_timestamp": datetime.datetime.utcnow(),
-            "modified_timestamp": datetime.datetime.utcnow(),
-        },
-    ]
-
-
-@app.get("/fanvids/{fanvid_uuid}", response_model=Fanvid, tags=["Fanvids"])
-async def read_fanvid(fanvid_uuid: uuid.UUID):
-    return {
-        "uuid": fanvid_uuid,
-        "title": "Title",
-        "author": "Author",
-        "premiere_date": None,
-        "premiere_event": None,
-        "audio": None,
-        "length": datetime.timedelta(1),
-        "rating": "Gen",
-        "fandoms": [],
-        "summary": "Summary!",
-        "content_notes": ["Graphic depictions of violence"],
-        "urls": ["https://archiveofourown.org/works/29759784"],
-        "unique_identifiers": [],
-        "thumbnail_url": "https://i.vimeocdn.com/video/1072432518.jpg?mw=1280&mh=720&q=70",
-        "state": "active",
-        "created_timestamp": datetime.datetime.utcnow(),
-        "modified_timestamp": datetime.datetime.utcnow(),
-    }
-
-
-@app.put("/fanvids/{fanvid_uuid}", response_model=Fanvid, tags=["Fanvids"])
-async def update_fanvid(fanvid_uuid: uuid.UUID, fanvid: CreateOrUpdateFanvid):
-    fanvid_dict = fanvid.dict()
-    fanvid_dict.update(
-        {
-            "uuid": fanvid_uuid,
-            "created_timestamp": datetime.datetime.utcnow(),
-            "modified_timestamp": datetime.datetime.utcnow(),
-        }
-    )
-    return fanvid_dict
-
-
-@app.delete("/fanvids/{fanvid_uuid}", tags=["Fanvids"], status_code=204)
-async def delete_fanvid(fanvid_uuid: uuid.UUID):
-    return None
---
+++ fanviddb/fanvids/router.py
@@ -0,0 +1,88 @@
+import datetime
+import uuid
+from typing import List
+
+from fastapi import APIRouter
+from .models import CreateOrUpdateFanvid, Fanvid
+
+
+router = APIRouter()
+
+
+@router.post("/", response_model=Fanvid)
+async def create_fanvid(fanvid: CreateOrUpdateFanvid, status_code=201):
+    fanvid_dict = fanvid.dict()
+    fanvid_dict.update(
+        {
+            "uuid": uuid.uuid4(),
+            "created_timestamp": datetime.datetime.utcnow(),
+            "modified_timestamp": datetime.datetime.utcnow(),
+        }
+    )
+    return fanvid_dict
+
+
+@router.get("/", response_model=List[Fanvid])
+async def read_fanvid_list():
+    return [
+        {
+            "uuid": uuid.uuid4(),
+            "title": "Title",
+            "author": "Author",
+            "premiere_date": None,
+            "premiere_event": None,
+            "audio": None,
+            "length": datetime.timedelta(1),
+            "rating": "Gen",
+            "fandoms": [],
+            "summary": "Summary!",
+            "content_notes": ["Graphic depictions of violence"],
+            "urls": ["https://archiveofourown.org/works/29759784"],
+            "unique_identifiers": [],
+            "thumbnail_url": "https://i.vimeocdn.com/video/1072432518.jpg?mw=1280&mh=720&q=70",
+            "state": "active",
+            "created_timestamp": datetime.datetime.utcnow(),
+            "modified_timestamp": datetime.datetime.utcnow(),
+        },
+    ]
+
+
+@router.get("/{fanvid_uuid}", response_model=Fanvid)
+async def read_fanvid(fanvid_uuid: uuid.UUID):
+    return {
+        "uuid": fanvid_uuid,
+        "title": "Title",
+        "author": "Author",
+        "premiere_date": None,
+        "premiere_event": None,
+        "audio": None,
+        "length": datetime.timedelta(1),
+        "rating": "Gen",
+        "fandoms": [],
+        "summary": "Summary!",
+        "content_notes": ["Graphic depictions of violence"],
+        "urls": ["https://archiveofourown.org/works/29759784"],
+        "unique_identifiers": [],
+        "thumbnail_url": "https://i.vimeocdn.com/video/1072432518.jpg?mw=1280&mh=720&q=70",
+        "state": "active",
+        "created_timestamp": datetime.datetime.utcnow(),
+        "modified_timestamp": datetime.datetime.utcnow(),
+    }
+
+
+@router.put("/{fanvid_uuid}", response_model=Fanvid)
+async def update_fanvid(fanvid_uuid: uuid.UUID, fanvid: CreateOrUpdateFanvid):
+    fanvid_dict = fanvid.dict()
+    fanvid_dict.update(
+        {
+            "uuid": fanvid_uuid,
+            "created_timestamp": datetime.datetime.utcnow(),
+            "modified_timestamp": datetime.datetime.utcnow(),
+        }
+    )
+    return fanvid_dict
+
+
+@router.delete("/{fanvid_uuid}", status_code=204)
+async def delete_fanvid(fanvid_uuid: uuid.UUID):
+    return None
