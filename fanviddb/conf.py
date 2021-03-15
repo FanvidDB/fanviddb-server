93cd727053377865212e8a55ee4d03582dbdb1a2
sandalwoodbox <boxofsandalwood@gmail.com>
2021-03-14 21:49:19 -0700

    Allow explicitly setting test database url

--- fanviddb/conf.py
+++ fanviddb/conf.py
@@ -6,6 +6,10 @@ config = Config(".env")
 TESTING = config("TESTING", cast=bool, default=False)
 
 DATABASE_URL = config("DATABASE_URL", cast=databases.DatabaseURL)
-TEST_DATABASE_URL = DATABASE_URL.replace(database="test_" + DATABASE_URL.database)
+TEST_DATABASE_URL = config(
+    "TEST_DATABASE_URL",
+    cast=databases.DatabaseURL,
+    default=DATABASE_URL.replace(database="test_" + DATABASE_URL.database),
+)
 AUTH_SECRET_KEY = config("AUTH_SECRET_KEY")
 EMAIL_TOKEN_SECRET_KEY = config("EMAIL_TOKEN_SECRET_KEY")
