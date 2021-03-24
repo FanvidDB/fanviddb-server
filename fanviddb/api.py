7c3eb4ed3ce32d61e0f852f25e47d3934005a769
sandalwoodbox <boxofsandalwood@gmail.com>
2021-03-24 12:58:59 -0700

    Made frontend be generated via yarn command

--- fanviddb/api.py
+++ fanviddb/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.routing import Mount
 from starlette.routing import Route
 from starlette.staticfiles import StaticFiles
 
@@ -10,27 +11,29 @@ from .fanvids.router import router as fanvid_router
 
 app = FastAPI(
     routes=[
-        Route("/", endpoint=StaticFiles(directory="static", html=True), name="homepage")
+        Route("/", endpoint=StaticFiles(directory="frontend/build/", html=True), name="homepage"),
+        Mount("/static", app=StaticFiles(directory="frontend/build/static/", html=True), name="static"),
     ],
 )
+
 app.include_router(
     fanvid_router,
-    prefix="/fanvids",
+    prefix="/api/fanvids",
     tags=["Fanvids"],
 )
 app.include_router(
     auth_router,
-    prefix="/auth",
+    prefix="/api/auth",
     tags=["Auth"],
 )
 app.include_router(
     users_router,
-    prefix="/users",
+    prefix="/api/users",
     tags=["Users"],
 )
 app.include_router(
     api_key_router,
-    prefix="/api_keys",
+    prefix="/api/api_keys",
     tags=["API Keys"],
 )
 
