264b3526d1e0112add238a22170b6ac9580137fd
sandalwoodbox <boxofsandalwood@gmail.com>
2021-04-11 10:16:18 -0700

    Converted LocaleSelector to a function component

--- frontend/src/i18n/LocaleSelector.js
+++ frontend/src/i18n/LocaleSelector.js
@@ -2,31 +2,20 @@ import React from "react";
 import { Radio } from "antd";
 import PropTypes from "prop-types";
 
-const DEFAULT_LOCALE = "en-US";
-
-class LocaleSelector extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locale: DEFAULT_LOCALE,
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Radio.Group value={this.props.locale} onChange={this.props.onChange}>
-          <Radio.Button key="en-US" value="en-US">
-            English
-          </Radio.Button>
-          <Radio.Button key="zh-CN" value="zh-CN">
-            中文
-          </Radio.Button>
-        </Radio.Group>
-      </div>
-    );
-  }
-}
+const LocaleSelector = (props) => {
+  return (
+    <div>
+      <Radio.Group value={props.locale} onChange={props.onChange}>
+        <Radio.Button key="en-US" value="en-US">
+          English
+        </Radio.Button>
+        <Radio.Button key="zh-CN" value="zh-CN">
+          中文
+        </Radio.Button>
+      </Radio.Group>
+    </div>
+  );
+};
 
 LocaleSelector.propTypes = {
   onChange: PropTypes.func.isRequired,
