5753c33ab06373c237074765bece84e32d480f81
sandalwoodbox <boxofsandalwood@gmail.com>
2021-05-01 16:20:09 -0700

    Added pagination to fanvid list

--- frontend/src/fanvids/FanvidList.js
+++ frontend/src/fanvids/FanvidList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { List, Skeleton, Alert, Space, Typography, Tag } from "antd";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import { Localized } from "@fluent/react";
 import { callApi } from "../api";
 import { contentNotes, ratings } from "./constants";
@@ -8,17 +8,26 @@ import _ from "lodash";
 
 const { Text, Paragraph } = Typography;
 
+const PAGE_SIZE = 10;
+
 const FanvidList = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [totalCount, setTotalCount] = useState(0);
   const [fanvids, setFanvids] = useState([]);
   const [errors, setErrors] = useState([]);
   const location = useLocation();
+  const history = useHistory();
 
   const searchParams = new URLSearchParams(location.search);
+  let currentPage = parseInt(searchParams.get("page"));
+  if (isNaN(currentPage) || currentPage < 1) currentPage = 1;
 
   useEffect(() => {
     setIsLoading(true);
-    callApi("/api/fanvids", "GET").then(({ ok, json }) => {
+    callApi(
+      `/api/fanvids?limit=${PAGE_SIZE}&offset=${PAGE_SIZE * (currentPage - 1)}`,
+      "GET"
+    ).then(({ ok, json }) => {
       setIsLoading(false);
       if (!ok) {
         setErrors([
@@ -26,9 +35,16 @@ const FanvidList = () => {
         ]);
       } else {
         setFanvids(json.fanvids);
+        setTotalCount(json.total_count);
       }
     });
-  }, [searchParams.page]);
+  }, [currentPage]);
+
+  const onChangePage = (page) => {
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set("page", page);
+    history.push(`${location.path || ""}?${searchParams.toString()}`);
+  };
 
   if (!_.isEmpty(errors)) {
     return (
@@ -49,6 +65,13 @@ const FanvidList = () => {
       itemLayout="vertical"
       loading={isLoading}
       dataSource={fanvids}
+      pagination={{
+        position: "both",
+        pageSize: PAGE_SIZE,
+        total: totalCount,
+        current: currentPage,
+        onChange: onChangePage,
+      }}
       renderItem={(fanvid) => (
         <List.Item
           actions={[
