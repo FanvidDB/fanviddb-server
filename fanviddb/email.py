6e69eb04f5581faf6e434ab6257cf991abbff2a9
sandalwoodbox <boxofsandalwood@gmail.com>
2023-11-25 11:17:31 -0800

    Switched to Postmark for email sending

--- fanviddb/email.py
+++ fanviddb/email.py
@@ -1,8 +1,7 @@
 from typing import List
 
-from python_http_client.exceptions import UnauthorizedError  # type: ignore
-from sendgrid import SendGridAPIClient  # type: ignore
-from sendgrid.helpers.mail import Mail  # type: ignore
+from pystmark import Message  # type: ignore
+from pystmark import send  # type: ignore
 
 from fanviddb import conf
 
@@ -11,36 +10,29 @@ class EmailSendFailed(Exception):
     pass
 
 
-def send_email(from_email: str, to_emails: List[str], subject: str, content: str):
-    if conf.TESTING:
-        send_dummy_email(from_email, to_emails, subject, content)
-    elif conf.SENDGRID_API_KEY:
-        send_sendgrid_email(from_email, to_emails, subject, content)
-    else:
-        send_dummy_email(from_email, to_emails, subject, content)
-
-
-def send_dummy_email(from_email: str, to_emails: List[str], subject: str, content: str):
-    print("-" * 79)
-    print(f"From: {from_email}")
-    print(f"To: {','.join(to_emails)}")
-    print(f"Subject: {subject}")
-    print("-" * 79)
-    print(content)
-    print("-" * 79)
-
-
-def send_sendgrid_email(
-    from_email: str, to_emails: List[str], subject: str, content: str
+def send_email(
+    from_email: str, to_emails: List[str], subject: str, content: str, tag: str = None
 ):
-    message = Mail(
-        from_email=from_email,
-        to_emails=to_emails,
-        subject=subject,
-        plain_text_content=content,
-    )
-    try:
-        sendgrid_client = SendGridAPIClient(conf.SENDGRID_API_KEY)
-        sendgrid_client.send(message)
-    except UnauthorizedError as exc:
-        raise EmailSendFailed("Sending email failed") from exc
+    if conf.POSTMARK_API_KEY:
+        message = Message(
+            sender=from_email,
+            to=to_emails,
+            subject=subject,
+            text=content,
+            tag=tag,
+            message_stream="outbound",
+        )
+        response = send(message, api_key=conf.POSTMARK_API_KEY)
+        try:
+            response.raise_for_status()
+        except Exception as exc:
+            raise EmailSendFailed("Sending email failed") from exc
+    else:
+        print("-" * 79)
+        print(f"From: {from_email}")
+        print(f"To: {','.join(to_emails)}")
+        print(f"Subject: {subject}")
+        print(f"Tag: {tag}")
+        print("-" * 79)
+        print(content)
+        print("-" * 79)
